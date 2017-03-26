1acaecc9734d9f83b46d54a4f5a31a37b314c700
jack <jackwood1@gmail.com>
2017-03-26 12:24:51 +0200

    First pass at views for Grade and Country

---
+++ includes/views/_country.php
@@ -0,0 +1,3 @@
+<li class="product">
+    <?php echo $country->country ?> <i><?php echo $country->country?></i> <b>$<?php echo $country->country ?></b>
+</li>
---
+++ includes/views/_grade.php
@@ -0,0 +1,3 @@
+<li class="product">
+    <?php echo $grade->grade ?> <i><?php echo $grade->description?></i> <b>$<?php echo $grade->scale ?></b>
+</li>
